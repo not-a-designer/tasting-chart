758efe66609e532034529d60bf088c75750506f4
not-a-designer <joel.johnson.design@gmail.com>
2022-12-12 01:00:16 -0700

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { RouteReuseStrategy } from '@angular/router';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { NgChartsModule} from 'ng2-charts';
+import { environment } from '../environments/environment'
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SavedProfilesService } from './services/saved-profiles.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), BrowserAnimationsModule, AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), 
+    IonicModule.forRoot(), 
+    BrowserAnimationsModule, 
+    AppRoutingModule
+  ],
   providers: [
     SavedProfilesService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
